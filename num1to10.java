a28e2b5e341dd2e3d26fc7c0bc1ac151a4ff7813
miguelbduffy <33195250+miguelbduffy@users.noreply.github.com>
2020-07-12 12:51:47 -0300

    Add files via upload

---
+++ num1to10.java
@@ -0,0 +1,22 @@
+
+public class num1to10 {
+
+	public static void main(String[] args) {
+		// Write a program to print numbers from 1 to 10.
+		
+		// FOR
+		for(int i = 1; i <= 10; i++) {
+			System.out.println(i);
+		}
+		
+		// WHILE
+		int i = 1;
+		
+		while(i <= 10) {
+			System.out.println(i);
+			i++;
+		}
+
+	}
+
+}
