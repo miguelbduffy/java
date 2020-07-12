2157a816e6945b7a1c097359aea69472e8b5256c
miguelbduffy <33195250+miguelbduffy@users.noreply.github.com>
2020-07-12 18:08:03 -0300

    Update factorial_number.java

--- factorial_number.java
+++ factorial_number.java
@@ -4,16 +4,18 @@ public class factorial_number {
 
 	public static void main(String[] args) {
 		// Write a program to find the factorial value of any number entered through the keyboard.
-		int num, i, factorial = 1;
 		
+		// declare int variables num, i and factorial (set it to 1).
+		int num, i, factorial = 1;
+		//ask the user tu input a number
 		Scanner in = new Scanner(System.in);
 		System.out.print("Input a number: ");
 			num = in.nextInt();
-		
+		// loop 1 up to the number, and do factorial times i
 		for(i = 1; i <= num; i++) {
 			factorial *= i;
-			
 		}
+		// print the result outside the loop
 		System.out.println("The factorial number of " + num + " is " + factorial + ".");
 		
 	  }
